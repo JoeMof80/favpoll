ba5a28b7b8cc07987becdd6f28519caa8ac1a2cf
Joseph Moffatt <joseph.moffatt@gmail.com>
2026-05-19 20:23:57 +0100

    storybook bug fixes

--- components/ui/button.stories.tsx
+++ components/ui/button.stories.tsx
@@ -8,7 +8,14 @@ const meta = {
   argTypes: {
     variant: {
       control: "select",
-      options: ["default", "outline", "secondary", "ghost", "destructive", "link"],
+      options: [
+        "default",
+        "outline",
+        "secondary",
+        "ghost",
+        "destructive",
+        "link",
+      ],
     },
     size: {
       control: "select",
